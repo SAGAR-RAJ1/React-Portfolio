0d4733fd0cf98087750a4aad7380f6372e266c20
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-07-17 17:43:45 +0530

    completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = (props) => {
     return (
         <div className="card" style={{ width: '18rem' }}>
-            <img src="../public/dev.webp" className="card-img-top" alt="..." />
+            <img src={props.img} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title" style={{ color: '#CBFC8F' }}>{props.title}</h5>
                 <p className="card-text">{props.text}</p>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,57 @@
-import '../styles/Portfolio.css'
-import Card from './Card';
-const Portfolio =()=>{
-    return(
-        <section className="third">
-        <h1 style={{ color: '#CBFC8F' }}>PORTFOLIO</h1>
-        <h3>Check Out My Projects</h3>
-         <br /><br /><br />
-        <h3 style={{ color: '#CBFC8F' }}>web design projects</h3>
-        <h3 style={{ color: '#CBFC8F' }}>. . . </h3>
-       <br /><br />
-        <div className="cards">
-        <Card title="WanderLust 2025" text="A Journey Beyond Destinations !" link="https://github.com/SAGAR-RAJ1/-WonderLust-Reloaded"/>
-        <Card title="Simon - Says" text="When Memory Meets Mayhem!" />
+import "../styles/Portfolio.css";
+import Card from "./Card";
+const Portfolio = () => {
+  return (
+    <section className="third">
+      <h1 style={{ color: "#CBFC8F" }}>PORTFOLIO</h1>
+      <h3>Check Out My Projects</h3>
+      <br />
+      <br />
+      <br />
+      <h3 style={{ color: "#CBFC8F" }}>web design projects</h3>
+      <h3 style={{ color: "#CBFC8F" }}>. . . </h3>
+      <br />
+      <br />
+      <div className="cards">
+        <Card
+          title="WanderLust 2025"
+          text="A Journey Beyond Destinations !"
+          link="https://github.com/SAGAR-RAJ1/-WonderLust-Reloaded"
+          img="/wander.png"
+        />
+        <Card
+          title="Portfolio"
+          text="You already looking at this"
+          link="https://github.com/SAGAR-RAJ1/Portfolio"
+          img="/portfolio.png"
+        />
+        <Card
+          title="Simon - Says"
+          text="When Memory Meets Mayhem!"
+          img="/simon.png"
+          link="https://github.com/SAGAR-RAJ1/My-Web-projects-10/tree/main/SimonSays_Game"
+        />
 
-         <Card title="Rock Paper Scissors" text="Battle of Fate,One Move at a Time!"/>
-         <Card title="Portfolio" text="You already looking at this"/>
-         <Card title="AI - Notes App" text="Smart notes with Gemini " link="https://github.com/SAGAR-RAJ1/ai-notes-app"/>
-        </div>
-        
-        </section>
-    )
-}
+        <Card
+          title="Rock Paper Scissors"
+          text="Battle of Fate,One Move at a Time!"
+          link="https://github.com/SAGAR-RAJ1/My-Web-projects-10/tree/main/rock%20paper"
+          img="/rock.png"
+        />
+        <Card
+          title="Life - Timer"
+          text="How much you lived!"
+          link="https://github.com/SAGAR-RAJ1/ai-notes-app"
+          img="/life.png"
+        />
+        <Card
+          title="AI - Notes App"
+          text="Smart notes with Gemini "
+          link="https://github.com/SAGAR-RAJ1/ai-notes-app"
+          img="/ai.png"
+        />
+      </div>
+    </section>
+  );
+};
 export default Portfolio;
