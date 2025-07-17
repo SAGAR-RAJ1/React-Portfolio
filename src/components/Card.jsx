00193a7917ab1a031c5619a41de05c697ffa755b
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-07-17 19:54:12 +0530

    update card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
+import "../styles/card.css";
 const Card = (props) => {
     return (
-        <div className="card" style={{ width: '18rem' }}>
+        <div className="card" >
             <img src={props.img} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title" style={{ color: '#CBFC8F' }}>{props.title}</h5>
